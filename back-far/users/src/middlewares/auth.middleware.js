51b31638fac7f4f037dee7c44a39ea0e8b71c402
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-02-08 05:47:46 -0400

    fix: add maven dependncy3

--- back-far/users/src/middlewares/auth.middleware.js
+++ back-far/users/src/middlewares/auth.middleware.js
@@ -3,15 +3,27 @@ require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
     const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Acceso denegado' });
+    console.log("Token recibido en backend:", token);
+
+    if (!token) {
+        return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado' });
+    }
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const tokenParts = token.split(' ');  
+        if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+            return res.status(400).json({ error: 'Token no válido' });
+        }
+
+        const verified = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
+        console.log("Usuario verificado:", verified);
+        
         req.user = verified;
         next();
     } catch (err) {
-        res.status(400).json({ error: 'Token no válido' });
+        return res.status(400).json({ error: 'Token no válido o expirado' });
     }
 };
+
 
 module.exports = verifyToken;
--- back-far/users/src/routes/auth.routes.js
+++ back-far/users/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, getRoles } = require('../controllers/auth.controller');
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
+router.get('/roles', getRoles);
 module.exports = router;
